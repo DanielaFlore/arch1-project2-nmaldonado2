c514c1b255fc4226b3a07b327548461440d52cf7
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-03-05 14:44:15 -0700

    Cleaned code in buttons header and updated makefile

--- project/blinky_buzzy_toy/buttons.h
+++ project/blinky_buzzy_toy/buttons.h
@@ -4,13 +4,13 @@
 #define BTN1 BIT0
 #define BTN2 BIT1
 #define BTN3 BIT2
-#define BTN4 BIT3/* switch1 is p1.3 */
+#define BTN4 BIT3
 
-#define BUTTONS BTN1 | BTN2 | BTN3 | BTN4/* only 1 switch on this board */
+#define BUTTONS BTN1 | BTN2 | BTN3 | BTN4
 
 void buttons_init();
 void buttons_interrupt_handler();
 
-extern char button_state_down, button_state_changed; /* effectively boolean */
+extern char button_state_down, button_state_changed; 
 
 #endif // included
