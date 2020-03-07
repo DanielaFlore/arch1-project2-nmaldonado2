acd0cd662f6d950c0eadc25b9eaf027eda254c56
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-03-07 07:48:34 -0700

    Changed values of defined constants

--- project/blinky_buzzy_toy/buzzer.h
+++ project/blinky_buzzy_toy/buzzer.h
@@ -6,6 +6,7 @@ void buzzer_set_period(short cycles, short mute);
 void buzzer_turn_off();
 void buzzer_turn_on();
 
-void set_sound();
+void set_sound(char music_note);
+void set_frequency(short cycles, short mute);
 
 #endif // included
