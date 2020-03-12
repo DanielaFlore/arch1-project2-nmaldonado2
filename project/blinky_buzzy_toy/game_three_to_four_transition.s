ca2360a826044ccadc55c129e9ffd1e8f9ad69a4
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-03-12 14:10:49 -0600

    Fixed bug in game transition that was not responding to button four press

--- project/blinky_buzzy_toy/game_three_to_four_transition.s
+++ project/blinky_buzzy_toy/game_three_to_four_transition.s
@@ -1,30 +1,22 @@
-
 	.text
 	.global game_three_interrupt_handler
 game_three_interrupt_handler:
 	
 	mov.b #0xf, r12
 	and.b &P2IN, r12 
-	mov.b #0xd, r13		;~BTN2
-	and.b #0xf, r13
-	cmp.b r12, r13
+	cmp.b #0xd, r12
 	jnz else_if
 	mov.b #4, &game_num
 	jmp end
 else_if:
-	mov.b #0x7, r13		;~BTN4
-	and.b #0xf, r13
-	cmp.b r12, r13
+	cmp.b #0x7, r12
 	jnz end
-	cmp.b #0, &red_on
-	jz first_nested_if
+	cmp.b #0, &red_on	;red_on - 0
+	jz second_nested_if	;jmp if red_on == 0
 	sub #0x5, &light_speed
-first_nested_if:
-	cmp.b #0, &red_on
-	jnz second_nested_if
-	mov.b #150, &light_speed
-	jmp end
 second_nested_if:
+	cmp.b #0, &red_on
+	jnz end
 	mov #15, r12
 	cmp.b &light_speed, r12
 	jc end
