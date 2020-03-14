565df235690d388c1257eed5f3ac1deed9e82f7a
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-03-13 23:11:22 -0600

    Added comments for stateMachine and interrupt handler

--- project/states_and_interrupts/p2_interrupt_handler.c
+++ project/states_and_interrupts/p2_interrupt_handler.c
@@ -1,26 +1,48 @@
+// Nichole Maldonado
+// This file determines which functions to
+// to call to handle the interrupt based
+// on the game number.
+
 #include <msp430.h>
 #include "buttons.h"
 #include "stateMachines.h"
-#include "led.h"
-#include "catch_red.h"
 
-/* Switch on P2 */
-void
-__interrupt_vec(PORT2_VECTOR) Port_2() {
+/*
+ * Function that calls the corresponding
+ * interrupt handler based on the game_num.
+ * Input: None.
+ * Outpu: None.
+ */
+void __interrupt_vec(PORT2_VECTOR) Port_2()
+{
+
+  // Handle interrupt based on button.
   if (P2IFG & BUTTONS) {
+
+    // Clear P2IFG for next interrupt interpretation.
     P2IFG &= ~BUTTONS;
+
+    // Fur elise interrupt handler.
     if (game_num == 1) {
       game_one_interrupt_handler();
     }
+
+    // Find frequency interrupt handler.
     else if (game_num == 2) {
-      frequency_recovery();
+      game_two_interrupt_handler();
     }
+
+    // Catch red interrupt handler.
     else if (game_num == 3) {
       game_three_interrupt_handler();
     }
+
+    // Simon interrupt handler.
     else {
       game_four_interrupt_handler();
     }
+
+    // Update IES.
     buttons_update_interrupt_sense();
   }
 }
--- project/states_and_interrupts/stateMachines.h
+++ project/states_and_interrupts/stateMachines.h
@@ -1,11 +1,26 @@
+// Nichole Maldonado
+// This is the header file for stateMachines.c
+// which determines which function to call to
+// handle the interrupt based on the game number.
+
 #ifndef stateMachine_included
 #define stateMachine_included
 
 extern char game_num;
 
+// Interrupt handlers for the corresponding
+// game numbers.
+
+// Interrupt handler for Fur Elise
 void game_three_interrupt_handler();
-void frequency_recovery();
+
+// Interrupt handler for Find Frequency
+void game_two_interrupt_handler();
+
+// Interrupt handler for Simon.
 void game_four_interrupt_handler();
+
+// Interrupt handler for Catch Red.
 void game_one_interrupt_handler();
 
 #endif // included
