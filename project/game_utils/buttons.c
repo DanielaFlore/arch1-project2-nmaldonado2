3a3ffef5008e0bca6e0c0975274d56edd6cfbc4d
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-03-13 20:25:14 -0600

    relocated led, buttons, and buzzer to new folder

---
+++ project/game_utils/buttons.c
@@ -0,0 +1,42 @@
+#include <msp430.h>
+#include "buttons.h"
+#include "led.h"
+
+char button_state_down, button_state_changed; /* effectively boolean */
+
+void
+buttons_update_interrupt_sense()
+{
+  // Button is up if bit == 1
+  // So if button is up, set P2IES to 1, to
+  // prepare for a high to low transition.
+  // (when button is pressed).
+  P2IES |= (P2IN & BUTTONS);
+
+  // Button is down if bit == 0
+  // So if button is down, set P2IES to 0, to
+  // prepare for a low to high transition.
+  // (when button is released).
+  P2IES &= (P2IN | ~BUTTONS); 
+}
+
+void 
+buttons_init()			/* setup switch */
+{
+  P2OUT |= BUTTONS;
+  
+  P2REN |= BUTTONS;		/* enables resistors for switches */
+  P2IE = BUTTONS;		/* enable interrupts from switches */
+  P2OUT |= BUTTONS;		/* pull-ups for switches */
+  P2DIR &= ~BUTTONS;		/* set switches' bits for input */
+  buttons_update_interrupt_sense();
+  led_change();
+}
+/*
+void
+buttons_interrupt_handler()
+{
+  buttons_update_interrupt_sense();
+  // If P2IN.x == 0, then button was pressed.
+  button_state_down = (P2IN & BUTTONS) ? 1 : 0;
+}*/
---
+++ project/game_utils/buttons.h
@@ -0,0 +1,14 @@
+#ifndef buttons_included
+#define buttons_included
+
+#define BTN1 BIT0
+#define BTN2 BIT1
+#define BTN3 BIT2
+#define BTN4 BIT3
+
+#define BUTTONS (BTN1 | BTN2 | BTN3 | BTN4)
+
+void buttons_init();
+void buttons_update_interrupt_sense();
+
+#endif // included
