73c2450b6945057c65940d7a23a4f9a0a24a9175
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-03-27 11:54:27 -0600

    Correctly fixed comment port number

--- project/game_utils/led.h
+++ project/game_utils/led.h
@@ -7,8 +7,8 @@
 #ifndef led_included
 #define led_included
 
-#define LED_RED BIT6               // P2.6
-#define LED_GREEN BIT0             // P2.0
+#define LED_RED BIT6               // P1.6
+#define LED_GREEN BIT0             // P1.0
 #define LEDS (BIT0 | BIT6)
 
 extern unsigned char red_on, green_on;
